0057c530335d074238c5527183aeb8613458011a
khali <asura.9660@gmail.com>
2024-03-26 12:30:06 +0100

    Modication for render

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,5 @@
 export const environment = {
     apiUrl: "https://sogenaccountprod-b65630290e4d.herokuapp.com/api"
+    // apiUrl: "https://localhost:7171/api"
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
     apiUrl: "https://sogenaccountprod-b65630290e4d.herokuapp.com/api"
+    // apiUrl: "https://localhost:7171/api"
 };
